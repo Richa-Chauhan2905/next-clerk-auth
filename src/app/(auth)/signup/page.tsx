01dfb0bf5914cd856cbefb03a1e5acdf6dfff31f
Richa-Chauhan2905 <chauhanricha2911@gmail.com>
2025-09-28 16:06:22 +0530

    sigin route added

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -114,7 +114,7 @@ const Signup = () => {
       <Card className="w-full max-w-md">
         <CardHeader>
           <CardTitle className="text-2xl font-bold text-center">
-            Sign Up for Todo Master
+            Sign Up
           </CardTitle>
         </CardHeader>
         <CardContent>
